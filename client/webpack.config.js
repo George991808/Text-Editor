66f4df0943f0fbc2081de769cc1786820a71927f
George991808 <gellis7@gmail.com>
2022-04-25 17:48:23 +0800

    edited dewscxription in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -34,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Jateeee",
-        short_name: "Jateee",
-        description: "This h",
+        name: "Jate",
+        short_name: "Jate",
+        description: "Text editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
